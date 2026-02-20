03ba7b4875e33d75c6c6f12143cff689908ad7d9
sinchana182005 <msinchana681@gmail.com>
2026-02-20 16:31:01 +0530

    Added new files

---
+++ src/RecursionProblems.java
@@ -0,0 +1,10 @@
+public class RecursionProblems {
+    static int sumOfDigits(int n){
+        if(n==0){
+            return 0;
+        }
+        return n % 10+sumOfDigits(n / 10);
+
+
+        }
+    }
--- src/StaticMethods.java
+++
@@ -1,9 +0,0 @@
-public class StaticMethods {
-    static int x = 200;
-    static void display(){
-        System.out.println("SNPSU_CS_IS");
-    }
-}
-class StaticDemo{
-    public static void main(String[] args)
-}
